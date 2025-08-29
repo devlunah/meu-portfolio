5f4f3c51de61de4e5fbcedf4504f5b81053ee063
devlunah <lunah.silvac@gmail.com>
2025-08-29 13:30:27 -0500

    mudanca db

--- teste/settings.py
+++ teste/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'teste.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "teste",
+        "USER": "root",
+        "PASSWORD": "222005Lu",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
